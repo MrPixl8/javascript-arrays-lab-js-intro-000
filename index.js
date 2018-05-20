b65ee45d79c9bf3a44ea065503bd69dde3f1ccbe
Jake Lovelace <jaglovelace@gmail.com>
2018-05-20 11:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(array, element) {
-   array.push(element)
+function destructivelyAppendKitten(name) {
+   kittens.push(name)
    return array
 }
